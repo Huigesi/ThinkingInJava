110abd506dae5dfe9d95021bac391860e33a26a2
Huigesi <791339970@qq.com>
2018-09-04 18:45:57 +0800

    se10 ex19

---
+++ src/section10/ex18/Ex18.java
@@ -0,0 +1,15 @@
+package section10.ex18;
+
+public class Ex18 {
+    Ex18() { System.out.println("Ex18()"); }
+    public static class Ex18Nest1 {
+        Ex18Nest1() { System.out.println("Ex18Nest1()"); }
+    }
+    private static class Ex18Nest2 {
+        Ex18Nest2() { System.out.println("Ex18Nest2()"); }
+    }
+    public static void main(String[] args) {
+        Ex18Nest1 en1 = new Ex18Nest1();
+        Ex18Nest2 en2 = new Ex18Nest2();
+    }
+}
