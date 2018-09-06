9a9bffc85ce0bbdcf895a7d3459c907e11b20ced
Huigesi <791339970@qq.com>
2018-09-06 19:27:56 +0800

    se13 ex2

---
+++ src/section14/ex2/ToyTest2.java
@@ -0,0 +1,55 @@
+package section14.ex2;
+interface HasBatteries {}
+
+interface Waterproof {}
+
+interface Shoots {}
+
+interface dow { }
+
+class Toy {
+	// With default constructor commented out will be
+	// unable to instantiate this Toy, super of FancyToy:
+	Toy() {}
+	Toy(int i) {}
+}
+
+class FancyToy extends Toy 
+	implements HasBatteries, Waterproof, Shoots,dow {
+		FancyToy() { super(1); }
+}
+
+public class ToyTest2 {
+	static void printInfo(Class cc) {
+		System.out.println("Class name: " + cc.getName() +
+			" is interface? [" + cc.isInterface() + "]");
+		System.out.println("Simple name: " + cc.getSimpleName());
+		System.out.println("Canonical name: " + cc.getCanonicalName());
+	}
+	public static void main(String[] args) {
+		Class c = null;
+		try {
+			c = Class.forName("section14.ex2.FancyToy");
+		} catch(ClassNotFoundException e) {
+			System.out.println("Can't find FancyToy");
+			System.exit(1);
+		}
+		printInfo(c);
+		for(Class face : c.getInterfaces())
+			printInfo(face);
+		Class up = c.getSuperclass();
+		Object obj = null;
+		try {
+			// Requires default constructor in order to
+			// create a super or Toy object:
+			obj = up.newInstance();
+		} catch(InstantiationException e) {
+			System.out.println("Cannot instantiate");
+			System.exit(1);
+		} catch(IllegalAccessException i) {
+			System.out.println("Cannot access");
+			System.exit(1);
+		}
+		printInfo(obj.getClass());
+	}
+}
---
+++ src/section14/ex4/Shapes4.java
@@ -0,0 +1,47 @@
+package section14.ex4;
+
+import java.util.Arrays;
+import java.util.List;
+
+abstract class Shape {
+	void draw() { System.out.println(this + ".draw()"); }
+	abstract public String toString();
+}
+
+class Circle extends Shape {
+	public String toString() { return "Circle"; }
+}
+
+class Square extends Shape {
+	public String toString() { return "Square"; }
+}
+
+class Triangle extends Shape {
+	public String toString() { return "Triangle"; }
+}
+
+class Rhomboid extends Shape {
+	public String toString() { return "Rhomboid"; }
+}
+
+public class Shapes4 {
+	public static void main(String[] args) {
+		// upcasting to Shape:
+		List<Shape> shapeList = Arrays.asList(
+			new Circle(), new Square(), new Triangle(), new Rhomboid()
+		);
+		// downcasting back to specific shape:
+		for(Shape shape : shapeList)
+			shape.draw();
+		Rhomboid r = new Rhomboid();
+		((Shape)r).draw();
+		// inconvertible types:
+        Shape s=(Shape)r ;
+		if (s instanceof Circle) {
+            ((Circle)s).draw();
+		}else {
+            System.out.println("(Shape)r is not a Circle");
+        }
+		// ((Circle)r).draw();
+	}
+}
