2919005681d95e792989639a38b521330b0ce74f
Huigesi <791339970@qq.com>
2018-09-06 22:18:05 +0800

    se13 ex8

--- src/section14/ex8/Ex8.java
+++ src/section14/ex8/Ex8.java
@@ -11,9 +11,18 @@ class C extends B {
 
 public class Ex8 {
     public static void printt(Object o) {
+        Object[] fields = o.getClass().getDeclaredFields();
+        if(fields.length == 0)
+            System.out.println(o.getClass() + " has no fields");
+        if(fields.length > 0) {
+            System.out.println("Field(s) of " + o.getClass() + ":");
+            for(Object obj : fields)
+                System.out.println(" " + obj);
+        }
         if (o.getClass().getSuperclass() != null) {
 
-            System.out.println(o.getClass() + " is a subclass of " + o.getClass().getSuperclass());
+            System.out.println(o.getClass() + " is a subclass of " + o.getClass().getSuperclass()
+                    + " DeclaredFields: "+o.getClass().getDeclaredFields());
 
             try {
                 printt(o.getClass().getSuperclass().newInstance());
