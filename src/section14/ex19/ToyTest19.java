a91c63d56bd3dc725f904d6536009839d4f3bd16
Huigesi <791339970@qq.com>
2018-09-07 14:20:37 +0800

    se15 ex2

---
+++ src/section14/ex19/ToyTest19.java
@@ -0,0 +1,39 @@
+package section14.ex19;
+
+import java.lang.reflect.InvocationTargetException;
+
+interface HasBatteries {}
+interface Waterproof {}
+interface Shoots {}
+
+class Toy {
+	Toy() {
+		System.out.println("Creating Toy() object");	
+	}
+	Toy(int i) {
+		System.out.println("Creating Toy(" + i + ") object");
+	}
+}
+
+class FancyToy extends Toy 
+	implements HasBatteries, Waterproof, Shoots {
+		FancyToy() { super(1); }
+}
+
+public class ToyTest19 {
+	public static void main(String[] args) {
+		// get appropriate constructor and create new instance:
+		try {
+			Toy.class.getDeclaredConstructor(int.class).newInstance(1);
+		// catch four exceptions:
+		} catch(NoSuchMethodException e) {
+			System.out.println("No such method: " + e);
+		} catch(InstantiationException e) {
+			System.out.println("Unable make Toy: " + e);
+		} catch(IllegalAccessException e) {
+			System.out.println("Unable access: " + e);
+		} catch(InvocationTargetException e) {
+			System.out.println("Target invocation problem: " + e);
+		}
+	}
+}
