fe869b83482886da5a2764c583d94c65cd99c8ba
Huigesi <791339970@qq.com>
2018-09-07 15:11:36 +0800

    se15 ex6

---
+++ src/section15/ex3/Ex3.java
@@ -0,0 +1,35 @@
+package section15.ex3;
+
+class SixTuple<A,B,C,D,E,F>{
+    public final A one;
+    public final B two;
+    public final C three;
+    public final D four;
+    public final E five;
+    public final F six;
+
+    public SixTuple(A one, B two, C three, D four, E five, F six) {
+        this.one = one;
+        this.two = two;
+        this.three = three;
+        this.four = four;
+        this.five = five;
+        this.six = six;
+    }
+
+    public String toString() {
+        return one+" "+two+" "+three+" "+four+" "+five+" "+six+"";
+    }
+}
+public class Ex3 {
+    static SixTuple<String,String,String,String,String,Integer> g(){
+        return new SixTuple<String,String,String,String,String,Integer>
+                ("a","b","c","d","e",Integer.valueOf(10));
+    }
+
+    public static void main(String[] args) {
+        SixTuple<String,String,String,String,String,Integer> six=g();
+        System.out.println(six);
+        System.out.println(g());
+    }
+}
---
+++ src/section15/ex6/RandomList.java
@@ -0,0 +1,35 @@
+package section15.ex6;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomList<T> {
+    private ArrayList<T> storage = new ArrayList<>();
+    private Random rand = new Random(47);
+
+    public void add(T item) {
+        storage.add(item);
+    }
+
+    public T select() {
+        return storage.get(rand.nextInt(storage.size()));
+    }
+
+    public static void main(String[] args) {
+        RandomList<String> rs = new RandomList<>();
+        RandomList<Integer> ri = new RandomList<>();
+        for (Integer i:new Integer[]{1,2,3,4,5,6}){
+            ri.add(i);
+        }
+        for (String s:("The quick brown for jumped over "+
+                "the lazy brown dog").split(" ")){
+            rs.add(s);
+
+        }
+        for (int i = 0; i < 11; i++) {
+            System.out.println(ri.select()+" ");
+        }
+
+    }
+}
