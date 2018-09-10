f6f2e5304c13048b9b31c5f5a6d9e080f15da1a7
Huigesi <791339970@qq.com>
2018-09-10 11:05:37 +0800

    se16 ex6

--- src/section16/ex6/Ex6.java
+++ src/section16/ex6/Ex6.java
@@ -5,7 +5,7 @@ import section16.BerylliumSphere;
 import java.util.Arrays;
 
 public class Ex6 {
-    public void createBery(int size1,int size2) {
+    public BerylliumSphere[][] createBery(int size1,int size2) {
         BerylliumSphere[][] berylliumSpheres=new BerylliumSphere[size1][size2];
         for (int i = 0; i < size1; i++) {
             berylliumSpheres[i] = new BerylliumSphere[size2];
@@ -13,10 +13,10 @@ public class Ex6 {
                 berylliumSpheres[i][j] = new BerylliumSphere();
             }
         }
-        System.out.println(Arrays.deepToString(berylliumSpheres));
+       return berylliumSpheres;
     }
 
     public static void main(String[] args) {
-        new Ex6().createBery(2,3);
+        System.out.println(Arrays.deepToString(new Ex6().createBery(3,4)));
     }
 }
