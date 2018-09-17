eea71f219abf02d2f26e721d6ab750589fdb81e1
Huigesi <791339970@qq.com>
2018-09-17 17:30:27 +0800

    se3,4

---
+++ src/Exercise/section2/ex7/CoinToss.java
@@ -0,0 +1,12 @@
+package Exercise.section2.ex7;
+
+import java.util.Random;
+
+public class CoinToss {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int coin = rand.nextInt();
+		if(coin % 2 == 0) System.out.println("heads");
+		else System.out.println("tails");		
+	}
+}
