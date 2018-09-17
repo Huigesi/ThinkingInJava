276c80d10b0cf433812cbee8e17849a61816b46f
Huigesi <791339970@qq.com>
2018-09-17 17:16:53 +0800

    se1

---
+++ src/Exercise/section1/DOTest2.java
@@ -0,0 +1,20 @@
+package Exercise.section1;
+public class DOTest2 {
+    public static void main(String[] args) {
+        class DataOnly {
+            int i;
+            double d;
+            boolean b;
+            void show() {
+                System.out.println(i);
+                System.out.println(d);
+                System.out.println(b);
+            }
+        }
+        DataOnly data = new DataOnly();
+        data.i = 234;
+        data.d = 2.1234545;
+        data.b = true;
+        data.show();
+    }
+}
---
+++ src/Exercise/section1/PrimitiveTest.java
@@ -0,0 +1,10 @@
+package Exercise.section1;
+
+public class PrimitiveTest {
+	static int i;
+	static char c;	
+	public static void main(String[] args) {		
+		System.out.println("int = " + i);
+		System.out.println("char = " + c);
+	}
+}
